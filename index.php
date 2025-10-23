65c18d78d5dc232282e83abf1f0dffb8e7613a91
beatriz287 <beatriz.saantoos01@gmail.com>
2025-10-23 12:26:47 -0300

    23/10

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+
+    <?php include "menu.php"?>
+
+    <div class="conteudo">
+        <h3>Integração PHP com MySQL<h3>
+            <p>
+                Nesta aula iremos estudar como integrar/conectar o PHP ao MySQL.
+            </p>
+
+            <p>
+                O objetivo desse projetp seré cadastrar alunos de uma escola
+            </p>
+    </div>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,11 @@
+    <div class="menu">
+        <div>
+            <a href="#">Aula BD</a>
+        </div>
+
+        <div>
+            <a href="#">Cadastrar</a>
+            <a href="#">Consultar</a>
+        </div>
+
+    </div>
