159496bdaa45f220debb2da10e2f5876729b7dfa
Etec <usuario@etec.com>
2025-10-30 12:02:59 -0300

    30/10

--- menu.php
+++ menu.php
@@ -6,6 +6,8 @@
         <div>
             <a href="aluno_cadastro.php">Cadastrar</a>
             <a href="aluno_consulta.php">Consultar</a>
+            <a href="prof_cadastro.php">Prof.Cadastrar</a>
+            <a href="prof_consulta.php">Prof.Consultar</a>
         </div>
 
     </div>
